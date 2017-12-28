b526fb7c22d5833c26f82f0380b904fefe3f30a8
vivekamin <aminvivek123@gmail.com>
2017-12-28 16:12:19 -0700

    Cross validation and pca implemeted

---
+++ knn_cross_validation.py
@@ -0,0 +1,82 @@
+import scipy as sc
+import pandas as pd
+import scipy.io as sio 
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.utils import shuffle
+
+
+knn_data = sio.loadmat('knn_data.mat')
+trainData = np.array(knn_data['train_data'])
+trainLabel = np.array(knn_data['train_label'])
+testData = np.array(knn_data['test_data'])
+testLabel = np.array(knn_data['test_label'])
+
+trainLabel = trainLabel.reshape((trainLabel.shape[0],))
+testLabel = testLabel.reshape((testLabel.shape[0],))
+trainLabel[0:int(trainLabel.shape[0]/5)].shape
+
+
+trainData, trainLabel = shuffle(trainData, trainLabel, random_state=0)
+i = 0
+trainingData = np.zeros((4000,trainData.shape[1]))
+accuracy_for_k = []
+k_s = []
+for l in range(0,9,1):
+    k = ((2*l) + 1)
+    total_accuracy = 0
+    for x in range(0,5,1):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        z = x * int(trainLabel.shape[0]/5)
+        trainingData = trainData[z+int(trainLabel.shape[0]/5):]
+        trainingLabel = trainLabel[z+int(trainLabel.shape[0]/5):]
+        temp = trainData[0:int(z)]
+        temp1 = trainLabel[0:int(z)]
+        if(x > 0):
+            trainingData = np.concatenate((trainingData,temp),axis = 0)
+            trainingLabel = np.concatenate((trainingLabel,temp1),axis = 0)
+
+        testData = trainData[z : z+int(trainLabel.shape[0]/5)]
+        testLabel = trainLabel[z : z+int(trainLabel.shape[0]/5)]
+        knn.fit(trainingData, trainingLabel)
+        pred = knn.predict(testData)
+        total_accuracy += accuracy_score(testLabel, pred)
+    print('Average Accuracy for ',k, total_accuracy/5)
+    k_s.append(k)
+    accuracy_for_k.append(total_accuracy/5)
+    
+    
+
+fig, ax = plt.subplots()
+A = k_s
+B = [ '%.5f' % elem for elem in accuracy_for_k ]
+
+index = accuracy_for_k.index(max(accuracy_for_k))
+k1 = A[index],
+y1 = max(accuracy_for_k),
+plt.plot(A,B)
+for xy in zip(k1, y1):                                       # <--
+    ax.annotate('(%s, %s)' % xy, xy=xy, textcoords='data',color='green') # <--
+#plt.plot(k_s,accuracy_for_k)
+plt.title("KNN Cross Validation")
+plt.xlabel("K(Number of neighbours)")
+plt.ylabel("Accuracy")
+plt.show()
+
+kclusters = A[index]
+print(y1)
+# trainData = np.array(knn_data['train_data'])
+# trainLabel = np.array(knn_data['train_label'])
+testData = np.array(knn_data['test_data'])
+testLabel = np.array(knn_data['test_label'])
+trainLabel = trainLabel.reshape(5000,)
+testLabel = testLabel.reshape(1598,)
+
+#Accuracy on Test Data
+knn = KNeighborsClassifier(n_neighbors=kclusters)
+knn.fit(trainData, trainLabel)
+pred = knn.predict(testData)
+print (accuracy_score(testLabel, pred))
+
---
+++ knn_cross_validation_pca.py
@@ -0,0 +1,96 @@
+import scipy as sc
+import pandas as pd
+import scipy.io as sio 
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
+
+readFile = sio.loadmat('knn_data.mat')
+
+kmeansData = np.array(readFile['train_data'])
+kmeansLabel = np.array(readFile['train_label'])
+
+kmeansTestData = np.array(readFile['test_data'])
+kmeansTestLabel = np.array(readFile['test_label'])
+
+def PCA(kmeansData1):
+    X_std = StandardScaler().fit_transform(kmeansData1)
+
+
+
+    mean_vec = np.mean(X_std, axis=0)
+    cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
+    cov_mat.shape
+    cov_mat = np.cov(X_std.T)
+    eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+    cor_mat1 = np.corrcoef(X_std.T)
+    eig_vals, eig_vecs = np.linalg.eig(cor_mat1)
+    cor_mat2 = np.corrcoef(kmeansData1.T)
+    eig_vals, eig_vecs = np.linalg.eig(cor_mat2)
+    u,s,v = np.linalg.svd(X_std.T)
+    eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
+    eig_pairs.sort()
+    eig_pairs.reverse()
+    matrix_w = np.hstack((eig_pairs[0][1].reshape(166,1),eig_pairs[1][1].reshape(166,1)));
+    for i in range(2,50,1):
+        matrix_w = np.hstack((matrix_w,eig_pairs[i][1].reshape(166,1)));
+    return X_std.dot(matrix_w)
+
+
+transformed = PCA(kmeansData)
+
+transformed, kmeansLabel = shuffle(transformed, kmeansLabel, random_state=0)
+
+i = 0
+trainingData = np.zeros((4000,transformed.shape[1]))
+accuracy_for_k = []
+k_s = []
+for l in range(0,9,1):
+    k = ((2*l) + 1)
+    total_accuracy = 0
+    for x in range(0,5,1):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        z = x * int(kmeansLabel.shape[0]/5)
+        trainingData = transformed[z+int(kmeansLabel.shape[0]/5):]
+        trainingLabel = kmeansLabel[z+int(kmeansLabel.shape[0]/5):]
+        temp = transformed[0:int(z)]
+        temp1 = kmeansLabel[0:int(z)]
+        if(x > 0):
+            trainingData = np.concatenate((trainingData,temp),axis = 0)
+            trainingLabel = np.concatenate((trainingLabel,temp1),axis = 0)
+
+        testData = transformed[z : z+int(kmeansLabel.shape[0]/5)]
+        testLabel = kmeansLabel[z : z+int(kmeansLabel.shape[0]/5)]
+        knn.fit(trainingData, trainingLabel)
+        pred = knn.predict(testData)
+        total_accuracy += accuracy_score(testLabel, pred)
+    print('Average Accuracy for ',k, total_accuracy/5)
+    k_s.append(k)
+    accuracy_for_k.append(total_accuracy/5)
+
+
+fig, ax = plt.subplots()
+A = k_s
+B = [ '%.5f' % elem for elem in accuracy_for_k ]
+
+index = accuracy_for_k.index(max(accuracy_for_k))
+k1 = A[index],
+y1 = max(accuracy_for_k),
+plt.plot(A,B)
+for xy in zip(k1, y1):
+    ax.annotate('(%s, %s)' % xy, xy=xy, textcoords='data',color='green')
+plt.title("KNN Cross Validation with PCA")
+plt.xlabel("K(Number of neighbours)")
+plt.ylabel("Accuracy")
+plt.show()
+
+k_clusters = A[index]
+print(k_clusters)
+transformed_test = PCA(kmeansTestData)
+knn = KNeighborsClassifier(n_neighbors=k_clusters)
+knn.fit(transformed, kmeansLabel)
+pred = knn.predict(transformed_test)
+print (accuracy_score(kmeansTestLabel, pred))
